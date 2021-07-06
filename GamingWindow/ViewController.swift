f48fd1e655032fbbf274f62cc66e6b48114cfb7e
p-x9 <50244599+p-x9@users.noreply.github.com>
2021-07-06 20:48:10 +0900

    Initial Commit

---
+++ GamingWindow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GamingWindow
+//
+//  Created by p-x9 on 2021/07/06.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
